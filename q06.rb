f68e0fe55c0dfc1ef72f2627e781b5322cfde205
tosada <tomoya.a.osada@gmail.com>
2016-05-06 01:22:15 +0900

    q06

---
+++ q06.rb
@@ -0,0 +1,19 @@
+N = 10000
+cnt = 0
+
+N.times do |i|
+  num = (i * 3) + 1
+
+  while num != 1
+    if i == num
+      cnt += 1
+      break
+    elsif num % 2 == 0
+      num = num / 2
+    else
+      num = (num * 3) + 1
+    end
+  end
+end
+
+puts cnt
